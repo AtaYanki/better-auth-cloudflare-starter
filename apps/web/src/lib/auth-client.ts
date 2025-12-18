eebaf8fb7996294054a5d4f7beaa9bd9aa803733
Ata <yavuz.atayanki@gmail.com>
2025-12-18 20:50:19 +0300

    initial commit

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,8 @@
+import type { auth } from "@my-better-t-appp/auth";
+import { createAuthClient } from "better-auth/react";
+import { polarClient } from "@polar-sh/better-auth";
+
+export const authClient = createAuthClient({
+	baseURL: import.meta.env.VITE_SERVER_URL,
+	plugins: [polarClient()],
+});
